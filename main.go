b76b8d4f77b027c6112184ac84b38f8cc73a6502
Muhammad Talha <talhaguy@gmail.com>
2021-09-16 00:21:10 -0700

    starting login handler

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var UserDb = make(map[string]string)
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/register", RegistrationHandler).Methods(http.MethodPost).Headers("Content-Type", "application/json")
+	router.HandleFunc("/login", LoginHandler).Methods(http.MethodPost).Headers("Content-Type", "application/json")
 
 	server := &http.Server{
 		Handler:      router,
@@ -35,8 +38,6 @@ func RegistrationHandler(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: validation
-
 	var registrationForm RegistrationForm
 	err = json.Unmarshal(jsonForm, &registrationForm)
 	if err != nil {
@@ -44,11 +45,87 @@ func RegistrationHandler(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// TODO: username validation
+	// TODO: password validation
+	// TODO: check if user exists in DB validation
+	// TODO: hash password
+
 	// TODO: registration
 	log.Printf("registering user %s", registrationForm.Username)
+	UserDb[registrationForm.Username] = registrationForm.Password
 
 	serverResponse := ServerResponse{
 		Status: "SUCCESS",
+	}
+	jsonResponse, err := json.Marshal(serverResponse)
+	if err != nil {
+		WriteErrorResponse(rw, http.StatusInternalServerError, "could not create response")
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(jsonResponse)
+}
+
+func LoginHandler(rw http.ResponseWriter, r *http.Request) {
+	log.Println("login handler")
+
+	// TODO: if already logged in (has refresh valid token header) skip
+
+	jsonForm, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		WriteErrorResponse(rw, http.StatusInternalServerError, "could not read body")
+		return
+	}
+
+	var loginForm LoginForm
+	err = json.Unmarshal(jsonForm, &loginForm)
+	if err != nil {
+		WriteErrorResponse(rw, http.StatusBadRequest, "could not parse json")
+		return
+	}
+
+	// TODO: username validation
+	// TODO: password validation
+
+	hashedPass, ok := UserDb[loginForm.Username]
+	if !ok {
+		log.Printf("user %s does not exist", loginForm.Username)
+		WriteErrorResponse(rw, http.StatusUnauthorized, "wrong credentials")
+		return
+	}
+
+	// TODO: unhash password
+
+	if loginForm.Password != hashedPass {
+		log.Printf("wrong password for user %s", loginForm.Username)
+		WriteErrorResponse(rw, http.StatusUnauthorized, "wrong credentials")
+		return
+	}
+
+	log.Printf("auth successful for user %s", loginForm.Username)
+
+	// TODO: create access JWT
+	accessJWT := "access-jwt-12345"
+
+	// TODO: create refresh JWT
+	refreshJWT := "refresh-jwt-12345"
+
+	http.SetCookie(rw, &http.Cookie{
+		Name:     "refresh-token",
+		Value:    refreshJWT,
+		Expires:  time.Now().Add(time.Minute * 30),
+		HttpOnly: true,
+	})
+
+	serverResponse := &LoginServerResponse{
+		ServerResponse: ServerResponse{
+			Status: "SUCCESS",
+		},
+		Data: LoginServerResponseData{
+			AccessToken: accessJWT,
+		},
 	}
 	jsonResponse, err := json.Marshal(serverResponse)
 	if err != nil {
@@ -81,7 +158,21 @@ type RegistrationForm struct {
 	Password string `json:"password"`
 }
 
+type LoginForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type ServerResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+
+type LoginServerResponse struct {
+	ServerResponse
+	Data LoginServerResponseData `json:"data"`
+}
+
+type LoginServerResponseData struct {
+	AccessToken string `json:"accessToken"`
 }
